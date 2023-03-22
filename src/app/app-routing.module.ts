0275528ae6aed2e3d835c158a7a403431d8cd4fa
Abhi Shah <ashah@argusoft.com>
2023-03-22 19:08:17 +0530

    Component add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { MerchantsComponent } from './merchants/merchants.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'merchants', component: MerchantsComponent, children:[]},
+  { path: 'products', component: ProductsComponent, children:[]},
+  { path: 'orders', component: OrdersComponent, children:[]},
+  { path: 'inventory', component: InventoryComponent, children:[]},
+  { path: '', redirectTo: 'dashboard', pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MerchantsComponent } from './merchants/merchants.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    MerchantsComponent,
+    ProductsComponent,
+    OrdersComponent,
+    InventoryComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
