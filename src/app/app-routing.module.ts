b12663b3520e53eb33e11b739b7eab49ea25fa55
Abhi Shah <abhishah3102@gmail.com>
2023-03-27 18:55:54 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,45 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryComponent } from './inventory/inventory.component';
+import { MerchantAddComponent } from './merchants/merchant-add/merchant-add.component';
+import { MerchantDetailsComponent } from './merchants/merchant-details/merchant-details.component';
+import { MerchantHomeComponent } from './merchants/merchant-home/merchant-home.component';
+import { MerchantStartComponent } from './merchants/merchant-start/merchant-start.component';
+import { MerchantUpdateComponent } from './merchants/merchant-update/merchant-update.component';
 import { MerchantsComponent } from './merchants/merchants.component';
 import { OrdersComponent } from './orders/orders.component';
 import { ProductsComponent } from './products/products.component';
+import { ReportsComponent } from './reports/reports.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'merchants', component: MerchantsComponent, children:[]},
-  { path: 'products', component: ProductsComponent, children:[]},
-  { path: 'orders', component: OrdersComponent, children:[]},
-  { path: 'inventory', component: InventoryComponent, children:[]},
-  { path: '', redirectTo: 'dashboard', pathMatch: "full"},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'merchants/add', component: MerchantAddComponent },
+  { path: 'merchants/edit/:id', component: MerchantUpdateComponent },
+  {
+    path: 'merchants',
+    component: MerchantsComponent,
+    children: [
+      {
+        path: '',
+        component: MerchantHomeComponent,
+        children: [
+          { path: ':id', component: MerchantDetailsComponent },
+          { path: '', component: MerchantStartComponent },
+        ],
+      },
+    ],
+  },
+  { path: 'products', component: ProductsComponent, children: [] },
+  { path: 'orders', component: OrdersComponent, children: [] },
+  { path: 'inventory', component: InventoryComponent, children: [] },
+  { path: 'reports', component: ReportsComponent, children: [] },
+  { path: 'settings', component: SettingsComponent, children: [] },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
