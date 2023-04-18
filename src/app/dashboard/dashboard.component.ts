bfe6e3ecd22005658a6dfdda3a72948c8985e4de
Abhi Shah <abhishah3102@gmail.com>
2023-04-18 11:53:36 +0530

    Dashboard Dynamic Data Testing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Data } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
-import { Subject } from 'rxjs';
+import { AppModule } from '../app.module';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,18 +9,44 @@ import { Subject } from 'rxjs';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  dataFetched: boolean = false;
+  public chart: any;
+  public chart1: any;
+  sales = {};
+
+  constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
     this.createChart();
     this.createChart1();
+    this.getSalesData()
+      // .then(() => {
+      //   this.dataFetched = true;
+      // })
+      // .catch(() => {
+      //   console.log('something went wrong');
+      // });
   }
 
-  
-  public chart: any;
-  public chart1: any;
+  getSalesData() {
+    const promise = new Promise((resolve, reject) => {
+      this.http.get(AppModule.apiLink + 'utility/sales').subscribe(
+        (data) => {
+          this.sales = data;
+          resolve(data);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+    return promise;
+  }
+
   createChart() {
     this.chart = new Chart('MyChart', {
       type: 'bar', //this denotes tha type of chart
-      
+
       data: {
         // values on X-Axis
         labels: [
@@ -54,7 +80,7 @@ export class DashboardComponent implements OnInit {
   createChart1() {
     this.chart1 = new Chart('MyChart1', {
       type: 'line', //this denotes tha type of chart
-      
+
       data: {
         // values on X-Axis
         labels: [
@@ -87,77 +113,76 @@ export class DashboardComponent implements OnInit {
   }
 
   dtOptions = {};
-  
+
   data = [
     {
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz",
-      "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-          "lat": "-37.3159",
-          "lng": "81.1496"
-        }
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+        geo: {
+          lat: '-37.3159',
+          lng: '81.1496',
+        },
       },
-      "phone": "1-770-736-8031 x56442",
-      "website": "hildegard.org",
-      "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-      }
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org',
+      company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets',
+      },
     },
     {
-      "id": 2,
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv",
-      "address": {
-        "street": "Victor Plains",
-        "suite": "Suite 879",
-        "city": "Wisokyburgh",
-        "zipcode": "90566-7771",
-        "geo": {
-          "lat": "-43.9509",
-          "lng": "-34.4618"
-        }
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      address: {
+        street: 'Victor Plains',
+        suite: 'Suite 879',
+        city: 'Wisokyburgh',
+        zipcode: '90566-7771',
+        geo: {
+          lat: '-43.9509',
+          lng: '-34.4618',
+        },
       },
-      "phone": "010-692-6593 x09125",
-      "website": "anastasia.net",
-      "company": {
-        "name": "Deckow-Crist",
-        "catchPhrase": "Proactive didactic contingency",
-        "bs": "synergize scalable supply-chains"
-      }
+      phone: '010-692-6593 x09125',
+      website: 'anastasia.net',
+      company: {
+        name: 'Deckow-Crist',
+        catchPhrase: 'Proactive didactic contingency',
+        bs: 'synergize scalable supply-chains',
+      },
     },
     {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "username": "Samantha",
-      "email": "Nathan@yesenia.net",
-      "address": {
-        "street": "Douglas Extension",
-        "suite": "Suite 847",
-        "city": "McKenziehaven",
-        "zipcode": "59590-4157",
-        "geo": {
-          "lat": "-68.6102",
-          "lng": "-47.0653"
-        }
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      address: {
+        street: 'Douglas Extension',
+        suite: 'Suite 847',
+        city: 'McKenziehaven',
+        zipcode: '59590-4157',
+        geo: {
+          lat: '-68.6102',
+          lng: '-47.0653',
+        },
       },
-      "phone": "1-463-123-4447",
-      "website": "ramiro.info",
-      "company": {
-        "name": "Romaguera-Jacobson",
-        "catchPhrase": "Face to face bifurcated interface",
-        "bs": "e-enable strategic applications"
-      }
-    }
+      phone: '1-463-123-4447',
+      website: 'ramiro.info',
+      company: {
+        name: 'Romaguera-Jacobson',
+        catchPhrase: 'Face to face bifurcated interface',
+        bs: 'e-enable strategic applications',
+      },
+    },
   ];
-
 }
