08b1988441007b90f3d1db40dcd0d5036451bc52
Abhi Shah <abhishah3102@gmail.com>
2023-05-02 17:59:05 +0530

    Check Connection Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrServices } from './services/toastr.services';
+import { HttpClient } from '@angular/common/http';
+import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iMS-frontend';
+
+  constructor(private http: HttpClient, private toastr: ToastrServices) {}
+
+  ngOnInit(): void {
+    this.checkConnection();
+  }
+
+  getConnection() {
+    const promise = new Promise((reject, resolve) => {
+      this.http.get(AppModule.apiLink).subscribe(
+        (data) => {
+          if (data['success'] == true) {
+          }
+        },
+        (error) => {
+          this.toastr.error('Trying to reconnect...');
+        }
+      );
+    });
+    return promise;
+  }
+
+  checkConnection() {
+    setInterval(() => {
+      this.getConnection();
+    }, 30000);
+  }
 }
